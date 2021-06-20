359f61542f1665225cffb27dab005785c91c85a2
adilvelizade0 <adilvalizada@outlook.com>
2021-06-20 11:16:55 +0400

    firstCommit

---
+++ src/style/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const MainDiv = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export { MainDiv };
